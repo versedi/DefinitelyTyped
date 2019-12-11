a9329d9e474525c816ea45c23a77cf36c6ca06a2
Tadeusz Stepnikowski <t.stepnikowski@kfi.pl>
2019-12-11 09:34:14 +0100

    Types for trevoreyre/autocomplete-js 2.0.3

---
+++ types/autocomplete-js/README.md
@@ -0,0 +1,14 @@
+# Installation
+> `npm install --save @types/autocomplete-js`
+
+# Summary
+This package contains type definitions for @trevoreyre/autocomplete-js (https://github.com/trevoreyre/autocomplete/tree/master/packages/autocomplete-js).
+
+# Details
+Files were exported from https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/bootstrap
+
+Additional Details
+ * Last updated: Mon, 11 Dec 2019 09:09:27 GMT
+
+# Credits
+These definitions were written by versedi <https://github.com/versedi>.
---
+++ types/autocomplete-js/index.d.ts
@@ -0,0 +1,81 @@
+// Type definitions for autocomplete-js
+// Project: https://github.com/trevoreyre/autocomplete/, https://autocomplete.trevoreyre.com/
+// Definitions by: versedi <https://github.com/versedi>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
+
+export as namespace Autocomplete;
+
+
+/**
+ * * Creates a props object with overridden toString function. toString returns an attributes
+ string in the format: `key1="value1" key2="value2"` for easy use in an HTML string.
+ */
+export class Props {
+    /**
+     * @param index
+     * @param selectedIndex
+     * @param baseClass
+     */
+    constructor(index: string, selectedIndex: string, baseClass: string);
+    toString(): string
+}
+
+
+// --------------------------------------------------------------------------------------
+// Options Interfaces
+// --------------------------------------------------------------------------------------
+
+export interface AutocompleteJsOptions {
+    /**
+     * The search function to be executed on user input. Can be a synchronous function or a Promise.
+     * @param input
+     */
+    search(input: string): Function|Promise<string>,
+
+    /**
+     * Fired when user submits result. Receives result as argument.
+     * @param result
+     */
+    onSubmit(result: object): void,
+
+    baseClass?: string,
+
+    /**
+     * Controls whether first result should be highlighted after input
+     * Defaults to false
+     */
+    autoSelect?: boolean,
+
+    /**
+     * For complex search results, this function is executed to get the value to display in the input
+     * @param result
+     */
+    getResultValue? (result: object): void,
+
+    /**
+     *
+     * @param result - The result value returned from your search function
+     * @param props - An object containing generated attributes for the result item, which are expected to be set on your li element. The object has a custom toString function which lets you easily serialize it to a String of HTML attributes in the form attribute1="value1" attribute2="value2". This way, you don't have to worry about generating the proper IDs, classes, and ARIA attributes yourself.
+     */
+    renderResult? (result: object, props: Props): string|Element
+}
+
+export interface EventHandlers {
+    handleInput: Function,
+    handleKeyDown: Function,
+    handleBlur: Function,
+    handleResultMouseDown: Function,
+    handleResultClick: Function,
+}
+
+
+// --------------------------------------------------------------------------------------
+// Autocomplete
+// --------------------------------------------------------------------------------------
+
+declare global {
+    interface Autocomplete<TElement = HTMLElement> {
+        constructor(inputSelector: string, options?: AutocompleteJsOptions);
+    }
+}
