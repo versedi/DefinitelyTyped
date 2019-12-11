1c382ee416ef4bfa29226f472514c5f5d2b1ea7e
Tadeusz Stepnikowski <t.stepnikowski@kfi.pl>
2019-12-11 10:24:35 +0100

    Formatting, fixes to pass npm run lint

--- types/trevoreyre__autocomplete/index.d.ts
+++ types/trevoreyre__autocomplete/index.d.ts
@@ -7,8 +7,8 @@
 export as namespace Autocomplete;
 
 /**
- * * Creates a props object with overridden toString function. toString returns an attributes
- string in the format: `key1="value1" key2="value2"` for easy use in an HTML string.
+ * Creates a props object with overridden toString function. toString returns an attributes
+ * string in the format: `key1="value1" key2="value2"` for easy use in an HTML string.
  */
 export class Props {
     /**
@@ -17,7 +17,7 @@ export class Props {
      * @param baseClass string
      */
     constructor(index: string, selectedIndex: string, baseClass: string);
-    toString(): string
+    toString(): string;
 }
 
 // --------------------------------------------------------------------------------------
@@ -29,69 +29,69 @@ export interface AutocompleteOptions {
      * The search function to be executed on user input. Can be a synchronous function or a Promise.
      * @param input
      */
-    search(input: string): Function|Promise<string>,
+    search(input: string): () => string | Promise<string>;
 
     /**
      * Controls whether first result should be highlighted after input
      * Defaults to false, optional
      */
-    autoSelect?: boolean,
+    autoSelect?: boolean;
 
     /**
      * Sets the value of the calling component's input element
      */
-    setValue? (): Function,
+    setValue?(): () => void;
 
     /**
      * Sets attributes on the calling component's input element
      */
-    setAttribute? (): Function,
+    setAttribute?(): () => void;
 
     /**
      * Fired when the results list is updated. Receives results (Array), and selectedIndex (Number) as arguments.
      */
-    onUpdate? (): Function,
+    onUpdate?(): () => void;
 
     /**
      * Fired when user submits result. Receives result as argument.
      * @param result
      */
-    onSubmit(result: object): void,
+    onSubmit(result: object): void;
 
     /**
      *
      * @param result
      * @param props
      */
-    renderResult(result: object, props: Props): string
+    renderResult(result: object, props: Props): string;
 
     /**
      * Fired when the results list is shown
      */
-    onShow? (): Function,
+    onShow?(): () => void;
 
     /**
      * Fired when the results list is hidden
      */
-    onHide? (): Function,
+    onHide?(): () => void;
 
     /**
      * Fired if search is a Promise and hasn't resolved yet
      */
-    onLoading? (): Function,
+    onLoading?(): () => void;
 
     /**
      * Fired after asynchronous search function resolves
      */
-    onLoaded? (): Function,
+    onLoaded?(): () => void;
 }
 
 export interface EventHandlers {
-    handleInput: Function,
-    handleKeyDown: Function,
-    handleBlur: Function,
-    handleResultMouseDown: Function,
-    handleResultClick: Function,
+    handleInput: () => void;
+    handleKeyDown: () => void;
+    handleBlur: () => void;
+    handleResultMouseDown: () => void;
+    handleResultClick: () => void;
 }
 
 // --------------------------------------------------------------------------------------
@@ -99,7 +99,8 @@ export interface EventHandlers {
 // --------------------------------------------------------------------------------------
 
 declare global {
-    interface Autocomplete<TElement = HTMLElement> {
-        constructor(inputSelector: string, options?: AutocompleteOptions): Autocomplete;
+    class Autocomplete<TElement = HTMLElement> {
+        constructor(inputSelector: string, options?: AutocompleteOptions);
+        options: AutocompleteOptions;
     }
 }
